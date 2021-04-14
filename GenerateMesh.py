65c6831966a62bf3992418fbed4b90be39c24e07
Michael Katsoulis <michaelkatsoulis@gmail.com>
2021-04-14 15:54:23 +0200

    Generating mesh setup

---
+++ GenerateMesh.py
@@ -0,0 +1,47 @@
+"""
+Code by: Michael Katsoulis
+
+This code generates points withing a 2D/3D enviroment. 
+It takes in the size of the mesh and how many points need to be placed
+and then randomly places the points.
+
+Something like:
++--------------------------------------------------+
+|          xx        x                             |
+|    x        x               x   x                |
+|      x  x               x                        |
+|       x    x x     x xx                x         |
+|         x                  x                x    |
++--------------------------------------------------+
+
+"""
+import numpy as np
+
+
+
+def makeGrid(points, xdim, ydim, zdim =None, stepSize = 0.5):
+    """
+    Returns an array of sampled points all spaced out and structured
+
+    """
+    pass
+
+
+def generatePoints(total_num, xdim, ydim, zdim =None, stepSize = 0.5):
+    """
+    Returns the locations of the random points
+    """
+    pass
+
+def viewPoints(points):
+    """
+    Turns the points into something nice to look at
+    """
+    pass
+
+
+
+
+
+if __name__ == "__main__"dd:
+
