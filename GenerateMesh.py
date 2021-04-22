4657c72a3b0c7fc5421ff5df1abcfd93b1b8622b
Michael Katsoulis <michaelkatsoulis@gmail.com>
2021-04-22 15:02:45 +0200

    Updates to mesh generation and testing the module

--- GenerateMesh.py
+++ GenerateMesh.py
@@ -27,11 +27,28 @@ def makeGrid(points, xdim, ydim, zdim =None, stepSize = 0.5):
     pass
 
 
-def generatePoints(total_num, xdim, ydim, zdim =None, stepSize = 0.5):
+def generatePoints(total_num, x_dim, y_dim, z_dim =np.nan, stepSize = 0.5):
     """
     Returns the locations of the random points
+
+    
     """
-    pass
+
+
+    if np.isnan(z_dim):
+        x_pts = np.random.randint(x_dim+1, size=total_num)
+        y_pts = np.random.randint(y_dim+1, size=total_num)
+        pts = np.vstack((x_pts, y_pts)).T
+
+    else:
+        x_pts = np.random.randint(x_dim+1, size=total_num)
+        y_pts = np.random.randint(y_dim+1, size=total_num)
+        z_pts = np.random.randint(z_dim+1, size=total_num)
+        pts = np.vstack((x_pts, y_pts, z_pts)).T
+
+    return pts
+
+
 
 def viewPoints(points):
     """
@@ -43,5 +60,17 @@ def viewPoints(points):
 
 
 
-if __name__ == "__main__"dd:
+if __name__ == "__main__":
+    print("Test generate points 2D")
+    print("---------------------------------")
+    print(generatePoints(20, 100, 100))
+    print()
+
+    print("Test generatePoints 3D")
+    print("---------------------------------")
+    print(generatePoints(20, 100, 100, 100))
+    print()
+
+
+
 
