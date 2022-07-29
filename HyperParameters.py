862910674a42ce41aaec7ea8a297936d6d27baae
Michael <michaelkatsoulis@gmail.com>
2022-07-29 15:40:23 +0200

    modifying minimum distance in hyperparams

--- HyperParameters.py
+++ HyperParameters.py
@@ -20,14 +20,9 @@ config_path = "/home/kats/Code/PythonMeshManipulation/TunableReconstruction/mesh
 depth_est = aru_py_mesh.PyDepth(config_path)
 
 
-# K, R, t, _, _, _, _ = cv2.decomposeProjectionMatrix(data.calib.P_rect_10)
-# t = t[:3] / t[3]  # normalising translation vector
-# t.squeeze()
-
 NUM_INITIAL_FEATURES = 200
 LOWE_DISTANCE_RATIO = 0.8
 MAX_DISTANCE = 60  # meters
-MAX_DISPARITY = 0.5
 MAX_NUM_FEATURES_DETECT = 2000
 MIN_DISTANCE = 2
 TRIANGLE_SAMPLES_PER_PIX_SQUARED = 1 / 10 ** 2
@@ -36,6 +31,3 @@ DETECTOR = 'SIFT'
 INTERPOLATING_POINTS = 200
 NUM_SUPPORT_PTS_PER_OCCUPANCY_GRID = 2
 RESAMPLING_ITERATIONS = 3
-
-IMAGE_SHAPE = (720, 1280, 3)
-PREDICTION_SHAPE = (384, 640)
