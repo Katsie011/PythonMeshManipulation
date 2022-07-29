Fixed scaling error. Fixed bug in core_pts sizing

mesh_pydnet/error_analytics/pydnet_stereo_error.py
@@ -25,11 +25,14 @@ parser = argparse.ArgumentParser(description='Argument parser')
 parser.add_argument('--dataset', type=str, help='dataset to train on, kitti, or Husky', default='Husky')
 parser.add_argument('--datapath', type=str, help='path to the data',
                     default=r"/media/kats/Katsoulis3/Datasets/Husky/extracted_data/old_zoo/RouteC/2022_05_03_14_09_01")  # required=True)
-# parser.add_argument('--output_directory', type=str,
-#                     help='output directory for test disparities, if empty outputs to checkpoint folder',
-#                     default=output_directory)
 parser.add_argument('--checkpoint_dir', type=str, help='path to a specific checkpoint to load',
-                    default='/home/kats/Documents/My Documents/UCT/Masters/Code/PythonMeshManipulation/mesh_pydnet/checkpoints/Husky10K/Husky')
+                    # default='/home/kats/Documents/My Documents/UCT/Masters/Code/PythonMeshManipulation/mesh_pydnet/checkpoints/Husky10K/Husky')
+                    # default = '/home/kats/Code/training_monodepth/tmp/Husky2v0/Husky2v0/model-100000')
+                    # default='/home/kats/Code/training_monodepth/tmp/Husky2v1/Husky2v1/model-117950')
+                    # default = '/home/kats/Code/training_monodepth/tmp/Husky2v0/Husky2v0/model-117950')
+                    # default = "/home/kats/Code/training_monodepth/tmp/MonodepthHusky2v0_scratch/MonodepthHusky2v0/model-117950")
+                    default='/home/kats/Code/aru_sil_py/reconstruction/mesh_pydnet/checkpoints/MtrxGodzilla/PydnetTransferHusky10k/model'
+                            '-117950')
 parser.add_argument('--resolution', type=int, default=1, help='resolution [1:H, 2:Q, 3:E]')
 parser.add_argument('--save_predictions', type=bool, help="save predicted disparities to output directory",
                     default=True)
@@ -41,13 +44,12 @@ parser.add_argument('--height', dest='height', type=int, default=768, help='heig
 args = parser.parse_args()
 
 # pyvo = aru_py_vo.PyVO(args.vo_config)
-
-sift = img_ft.FeatureDetector(det_type='sift', max_num_ft=2000)
-orb = img_ft.FeatureDetector(det_type='orb', max_num_ft=2000)
+from reconstruction.HyperParameters import MIN_DISTANCE, MAX_DISTANCE, MAX_NUM_FEATURES_DETECT
+sift = img_ft.FeatureDetector(det_type='sift', max_num_ft=MAX_NUM_FEATURES_DETECT)
+orb = img_ft.FeatureDetector(det_type='orb', max_num_ft=MAX_NUM_FEATURES_DETECT)
 from reconstruction.VOComparison.aru_visual_odometry import pyvo
 import reconstruction.VOComparison.aru_visual_odometry as aru_vo_module
 
-MAX_DISTANCE = 100
 
 
 def get_stereo_disp(imgl, imgr, det=sift):
@@ -90,11 +92,11 @@ def pydnet_vs_aru_core_pts(prediction: np.ndarray, frame: int, dataset: husky.Da
     imgr_p1 = dataset.get_cam1(frame+1)
 
     if imgl.shape[:2] != prediction.shape:
-        # imgl = cv2.resize(imgl, prediction.shape[::-1])
-        # imgr = cv2.resize(imgr, prediction.shape[::-1])
-        # imgl_p1 = cv2.resize(imgl_p1, prediction.shape[::-1])
-        # imgr_p1 = cv2.resize(imgr_p1, prediction.shape[::-1])
-        prediction = cv2.resize(prediction, imgl.shape[:2][::-1])
+        imgl = cv2.resize(imgl, prediction.shape[::-1])
+        imgr = cv2.resize(imgr, prediction.shape[::-1])
+        imgl_p1 = cv2.resize(imgl_p1, prediction.shape[::-1])
+        imgr_p1 = cv2.resize(imgr_p1, prediction.shape[::-1])
+        # prediction = cv2.resize(prediction, imgl.shape[:2][::-1])
 
     Tr_stereo, pts_l, pts_r, pts_lp1 = pyvo.stereo_odometry(cv2.cvtColor(imgl, cv2.COLOR_BGR2GRAY),
                                                      cv2.cvtColor(imgr, cv2.COLOR_BGR2GRAY), dataset.left_times[frame].astype(int),
@@ -196,7 +198,7 @@ def predict_dataset_get_stereo_e(dataset, scaling_factor = 1, plots=True, verbos
     # if not get_orb and not get_sift:
     #     print("No feature detector chosen. Returning None")
     #     return None
-    disp_max = 100
+    disp_max = dataset.calib.baseline[0] * (dataset.calib.cam0_camera_matrix[0, 0]) / MIN_DISTANCE
     if max_frames is None:
         max_frames = dataset.num_frames - 1
     if get_sift:
@@ -240,7 +242,10 @@ def predict_dataset_get_stereo_e(dataset, scaling_factor = 1, plots=True, verbos
                 end_pred = time.time()
                 time_pred = start_pred - end_pred
                 # disparity = disp[0, :, :, 0].squeeze() * 0.3 * train_width
-                disparity = disp[0, :, :, 0].squeeze() * 0.3 * train_width * scaling_factor
+                # disparity = disp[0, :, :, 0].squeeze() * 0.3 * train_width * scaling_factor
+
+                # For some reason, removing the scaling factor of 0.3  increases accuracy and almost matches the stereo
+                disparity = disp[0, :, :, 0].squeeze() * train_width * scaling_factor
 
                 depth = dataset.calib.baseline[0] * (
                         dataset.calib.cam0_camera_matrix[0, 0] * train_width / dataset.img_shape[1]) / disparity
@@ -287,27 +292,33 @@ def predict_dataset_get_stereo_e(dataset, scaling_factor = 1, plots=True, verbos
 
 
                 if aru_core_pts:
-                    frame_core_e, frame_core_ratio ,core_pts, Tr_pydnet, Tr_stereo = pydnet_vs_aru_core_pts(prediction=fullsize_depth, frame=counter,
-                                                                    dataset=dataset, return_pts=True,
-                                                                    return_mse=False,
-                                                                    use_depth=False, verbose=False)
+                    frame_core_e, frame_core_ratio ,core_pts, Tr_pydnet, Tr_stereo = pydnet_vs_aru_core_pts(
+                        prediction=disparity, frame=counter,
+                        dataset=dataset, return_pts=True,
+                        return_mse=False,
+                        use_depth=False, verbose=False)
 
                     # frame_core_e, core_pts = pydnet_vs_aru_core_pts(prediction=disparity, frame=counter,
                     #                                                 dataset=dataset, return_pts=True,
                     #                                                 return_mse=False,
                     #                                                 use_depth=False)
+                    if (Tr_stereo is not None) and (Tr_stereo.shape == (4, 4)):
+                        coords_core = coords_core @ Tr_stereo
+                        # z-axis is fwd for img coords, x-axis is fwd in body coord sys
+                        x_core[counter] = coords_core[2][3]
+                        # x-axis is right img coords, y-axis is left in body coord sys
+                        y_core[counter] = -coords_core[0][3]
+                    else:
+                        print(f"Bad Tr stereo {Tr_stereo}")
 
-                    coords_core = coords_core @ Tr_stereo
-                    # z-axis is fwd for img coords, x-axis is fwd in body coord sys
-                    x_core[counter] = coords_core[2][3]
-                    # x-axis is right img coords, y-axis is left in body coord sys
-                    y_core[counter] = -coords_core[0][3]
-
-                    coords_pydnet = coords_pydnet @ Tr_pydnet
-                    # z-axis is fwd for img coords, x-axis is fwd in body coord sys
-                    x_pydnet[counter] = coords_pydnet[2][3]
-                    # x-axis is right img coords, y-axis is left in body coord sys
-                    y_pydnet[counter] = -coords_pydnet[0][3]
+                    if (Tr_pydnet is not None) and (Tr_pydnet.shape == (4, 4)):
+                        coords_pydnet = coords_pydnet @ Tr_pydnet
+                        # z-axis is fwd for img coords, x-axis is fwd in body coord sys
+                        x_pydnet[counter] = coords_pydnet[2][3]
+                        # x-axis is right img coords, y-axis is left in body coord sys
+                        y_pydnet[counter] = -coords_pydnet[0][3]
+                    else:
+                        print(f"Bad Tr stereo {Tr_pydnet}")
 
                     # mse_core[counter] = np.mean(frame_core_e ** 2)
                     core_es.append([np.mean(frame_core_e ** 2), frame_core_e.mean(), frame_core_ratio])
@@ -318,18 +329,25 @@ def predict_dataset_get_stereo_e(dataset, scaling_factor = 1, plots=True, verbos
 
 
                     if plots:
-                        core_pts[:,:2] = core_pts[:,:2]*disparity.shape[1]/fullsize_disparity.shape[1]
-                        core_disp_samples = disparity[core_pts[:, 1].astype(int), core_pts[:, 0].astype(int)]
+                        # Scaling down the u component for plotting
+                        # core_pts[:,0] = core_pts[:,0]*disparity.shape[1]/fullsize_disparity.shape[1]
+                        # core_pts[:,2]  = core_pts[:,2]*disparity.shape[1]/fullsize_disparity.shape[1]
+                        # core_pts[:,1] = core_pts[:,1]*disparity.shape[0]/fullsize_disparity.shape[0]
+                        #sampling predicted disp at points
+                        small_disp_sampled_at_core = disparity[core_pts[:, 1].astype(int), core_pts[:, 0].astype(int)]
+
+                        # Sizing down the left image for plotting
                         im_e_core = cv2.cvtColor(cv2.resize(imgl, disparity.shape[::-1]), cv2.COLOR_RGB2BGR)
-                        im_disp_core = cv2.cvtColor(cv2.resize(imgl, disparity.shape[::-1]), cv2.COLOR_RGB2BGR)
-                        im_disp_pred = cv2.cvtColor(cv2.resize(imgl, disparity.shape[::-1]), cv2.COLOR_RGB2BGR)
+                        im_disp_core, im_disp_pred = im_e_core.copy(), im_e_core.copy()
+
+                        # sizing the error to uint8
                         scaled_e = (np.abs(frame_core_e) * 255 / disp_max)
                         scaled_e[scaled_e > 255] = 255
                         e_colors = cv2.applyColorMap(scaled_e.astype(np.uint8), cv2.COLORMAP_JET)
                         scaled_s_dis = (np.abs(core_pts[:, 2]) * 255 / disp_max)
                         scaled_s_dis[scaled_s_dis > 255] = 255
                         stereo_disp_colors = cv2.applyColorMap(scaled_s_dis.astype(np.uint8), cv2.COLORMAP_INFERNO)
-                        scaled_disp_s = (np.abs(core_disp_samples) * 255 / disp_max)
+                        scaled_disp_s = (np.abs(small_disp_sampled_at_core) * 255 / disp_max)
                         scaled_disp_s[scaled_disp_s > 255] = 255
                         pred_disp_colors = cv2.applyColorMap(scaled_disp_s.astype(np.uint8), cv2.COLORMAP_INFERNO)
                         for i, c in enumerate(core_pts.astype(int)):
@@ -341,36 +359,23 @@ def predict_dataset_get_stereo_e(dataset, scaling_factor = 1, plots=True, verbos
                         cv2.imshow("Sift disparities", im_disp_core)
                         cv2.imshow("Predicted disparities", im_disp_pred)
 
-                # if get_orb:
-                #     print("\nEval on training size:")
-                #
-                #     mse_orb[counter], orb_pts = pydnet_vs_stereo_depth(prediction=disparity, frame=counter,
-                #                                                        dataset=dataset, det=orb, return_pts=True,
-                #                                                        use_depth=False)
-                #
-                #     # mse_orb[counter], orb_pts = pydnet_vs_stereo_depth(prediction=fullsize_disparity, frame=counter,
-                #     #                                                    dataset=dataset, det=orb, return_pts=True,
-                #     #                                                    use_depth=False)
-                #     if plots:
-                #         orb_im = cv2.cvtColor(cv2.resize(imgl, disparity.shape[::-1]), cv2.COLOR_RGB2BGR)
-                #         for c in orb_pts.astype(int):
-                #             orb_im = cv2.circle(orb_im, c, 2, (0, 0, 255), 2)
-                #
-                #         cv2.imshow("Orb Points", orb_im)
-
                 end_loop_time = time.time()
                 print(
                     f"\nTook {end_loop_time - start_loop_time:.3f}s to run one iteration with SIFT and ORB VO together")
 
                 if plots:
+                    color_disp = disparity * 255 / disp_max
+                    color_disp[color_disp > 255] = 255
+                    color_disp = color_disp.astype(np.uint8)
+
                     if get_sift:
                         img_and_depth = np.hstack((cv2.cvtColor(cv2.resize(im_disp_sift, disparity.shape[::-1]),
                                                                 cv2.COLOR_RGB2BGR), cv2.applyColorMap(
-                            (disparity * 255 / disparity.max()).astype(np.uint8), cv2.COLORMAP_INFERNO)))
+                            color_disp, cv2.COLORMAP_INFERNO)))
                     else:
                         img_and_depth = np.hstack((cv2.cvtColor(cv2.resize(imgl, disparity.shape[::-1]),
                                                                 cv2.COLOR_RGB2BGR), cv2.applyColorMap(
-                            (disparity * 255 / disp_max).astype(np.uint8), cv2.COLORMAP_INFERNO)))
+                            color_disp, cv2.COLORMAP_INFERNO)))
                     cv2.imshow("Input and disparity", img_and_depth)
                     cv2.waitKey(20)
 
@@ -391,23 +396,27 @@ if __name__ == "__main__":
     data_dir = "/home/kats/Datasets/Route A/2022_07_06_10_48_24"
     dataset_obj = husky.DatasetHandler(data_dir, time_tolerance=0.5)
     # get_stereo_disp(dataset_obj.get_cam0(0), dataset_obj.get_cam1(0))
-    max_frames = None
+    max_frames = 50
 
     # need to get the vo using the predicted disparity as well as using the disparity from stereo
 
-    es15,  [x_core15, y_core15], [x_pydnet15, y_pydnet15] = predict_dataset_get_stereo_e(dataset=dataset_obj,scaling_factor=1,
+    es1,  [x_core1, y_core1], [x_pydnet1, y_pydnet1] = predict_dataset_get_stereo_e(dataset=dataset_obj,scaling_factor=1,
                                                                                get_sift=False, aru_core_pts=True,
-                                                                               get_orb=False, plots=False,
+                                                                               get_orb=False, plots=True,
                                                                                max_frames=max_frames)
+
+    cv2.destroyAllWindows()
 
     es2,  [x_core2, y_core2], [x_pydnet2, y_pydnet2] = predict_dataset_get_stereo_e(dataset=dataset_obj,scaling_factor=2,
                                                                                get_sift=False, aru_core_pts=True,
-                                                                               get_orb=False, plots=False,
+                                                                               get_orb=False, plots=True,
                                                                                max_frames=max_frames)
+
+    cv2.destroyAllWindows()
 
     es25,  [x_core25, y_core25], [x_pydnet25, y_pydnet25] = predict_dataset_get_stereo_e(dataset=dataset_obj,scaling_factor=2.5,
                                                                                get_sift=False, aru_core_pts=True,
-                                                                               get_orb=False, plots=False,
+                                                                               get_orb=False, plots=True,
                                                                                max_frames=max_frames)
 
     # x_vo, y_vo = aru_vo_module.get_vo_path_on_dataset(dataset=dataset_obj)